ecbe39659c51064d2c234c1cab89fe5202b6e5a9
Pablo <pablotarriootero@gmail.com>
2020-04-09 13:47:38 +0200

    Scripts Base de Datos

---
+++ Scripts/CreacionAmbulatorio.sql
@@ -0,0 +1,290 @@
+create sequence secIngresos as int start with 1 increment by 1 minvalue 1;
+
+create sequence secAmbulatorios as int start with 1 increment by 1 minvalue 1;
+
+create sequence secHospitales as int start with 1 increment by 1 minvalue 1;
+
+create table ambulatorio (
+    codigoAmbulatorio int NOT NULL DEFAULT nextval('secAmbulatorios'),
+    nombre varchar(50) NOT NULL,
+    direccionPostal varchar(50) NOT NULL,
+    anoConstruccion varchar(4) NOT NULL DEFAULT extract(year from CURRENT_DATE),
+    provincia varchar(40) NOT NULL,
+    primary key (codigoAmbulatorio),
+    unique(nombre, provincia)
+);
+
+create table material (
+    codigoMaterial int NOT NULL,
+    tipo varchar(20) NOT NULL,
+    ambulatorio int NOT NULL DEFAULT currval('secAmbulatorios'),
+    descripcion varchar(200),
+    coste decimal(22, 2) NOT NULL,
+    fechaCompra date NOT NULL,
+    medico boolean NOT NULL,
+    primary key (codigoMaterial, ambulatorio),
+    foreign key (ambulatorio) references ambulatorio(codigoAmbulatorio) on delete cascade on update cascade
+);
+
+/* Función para asignar el codigo del material */
+create or replace function asignarCodigoMaterial() returns trigger as $acm$
+begin
+	if exists (select mt1.codigoMaterial
+			   from material as mt1
+			   where mt1.ambulatorio = new.ambulatorio
+			   and mt1.codigoMaterial >= all (select codigoMaterial
+											  from material as mt2
+											  where mt2.ambulatorio = mt1.ambulatorio))
+	then
+		begin
+			new.codigoMaterial := (select mt1.codigoMaterial + 1
+								   from material as mt1
+								   where mt1.ambulatorio = new.ambulatorio
+								   and mt1.codigoMaterial >= all (select codigoMaterial
+																  from material as mt2
+																  where mt2.ambulatorio = mt1.ambulatorio));
+		end;
+	else
+		begin
+			new.codigoMaterial := 1;
+		end;
+	end if;
+
+	return new;
+end;
+$acm$ language plpgsql;
+
+/* Trigger para insertar el número del material */
+create trigger triggerCodigoMaterial before insert on material
+for each row execute procedure asignarCodigoMaterial();
+
+create table subvencion (
+    codigoIngreso int NOT NULL DEFAULT nextval('secIngresos'),
+    ambulatorio int NOT NULL DEFAULT currval('secAmbulatorios'),
+    fecha date NOT NULL,
+    cantidad decimal(22, 2) NOT NULL DEFAULT 0.01,
+    motivo varchar(50) NOT NULL,
+    primary key (codigoIngreso, ambulatorio),
+    foreign key (ambulatorio) references ambulatorio(codigoAmbulatorio) on delete restrict on update cascade
+);
+
+create table donativo (
+    codigoIngreso int NOT NULL DEFAULT nextval('secIngresos'),
+    ambulatorio int NOT NULL DEFAULT currval('secAmbulatorios'),
+    fecha date NOT NULL,
+    cantidad decimal(22, 2) NOT NULL DEFAULT 0.01,
+    donante varchar(50),
+    primary key (codigoIngreso, ambulatorio),
+    foreign key (ambulatorio) references ambulatorio(codigoAmbulatorio) on delete restrict on update cascade
+);
+
+create table hospital (
+    codigoHospital int NOT NULL DEFAULT nextval('secHospitales'),
+    nombre varchar(50) NOT NULL,
+    direccion varchar(50) NOT NULL,
+    telefono char(9) NOT NULL,
+    provincia varchar(40) NOT NULL,
+    primary key (codigoHospital),
+    unique(nombre, provincia)
+);
+
+create table asociado (
+    ambulatorio int NOT NULL DEFAULT currval('secAmbulatorios'),
+    hospital int NOT NULL DEFAULT currval('secHospitales'),
+    distancia decimal(7, 2) NOT NULL DEFAULT 0 CHECK(distancia >= 0),
+    primary key (ambulatorio, hospital),
+    foreign key (ambulatorio) references ambulatorio(codigoAmbulatorio) on delete cascade on update cascade,
+    foreign key (hospital) references hospital(codigoHospital) on delete cascade on update cascade
+);
+
+create table especialidad (
+    nombre varchar(20) NOT NULL,
+    descripcion varchar(200),
+    primary key(nombre)
+);
+
+create table personalNoSanitario (
+    ambulatorio int NOT NULL DEFAULT currval('secAmbulatorios'),
+    dni char(9) NOT NULL,
+    nombre varchar(50) NOT NULL,
+    fechaIncorporacion date NOT NULL,
+    telefono char(9) NOT NULL,
+    sueldo decimal(7, 2) NOT NULL,
+    tipo varchar(20) NOT NULL,
+    primary key(ambulatorio, dni),
+    foreign key(ambulatorio) references ambulatorio(codigoAmbulatorio) on delete restrict on update cascade
+);
+
+create table personalAdministrador (
+    ambulatorio int NOT NULL DEFAULT currval('secAmbulatorios'),
+    personal char(9) NOT NULL,
+    contrasena varchar(30) NOT NULL,
+    primary key(ambulatorio, personal, contrasena),
+    foreign key(ambulatorio, personal) references personalNoSanitario(ambulatorio, dni) on delete restrict on update cascade
+);
+
+create table personalSanitario (
+    dni char(9) NOT NULL,
+    ambulatorio int NOT NULL DEFAULT currval('secAmbulatorios'),
+    nombre varchar(50) NOT NULL,
+    fechaIncorporacion date NOT NULL,
+    telefono char(9) NOT NULL,
+    sueldo decimal(7, 2) NOT NULL,
+    primary key(ambulatorio, dni),
+    foreign key(ambulatorio) references ambulatorio(codigoAmbulatorio) on delete restrict on update cascade
+);
+
+create table especializacionPersonal (
+    especialidad varchar(20) NOT NULL,
+    personal char(9) NOT NULL,
+    ambulatorio int NOT NULL DEFAULT currval('secAmbulatorios'),
+    primary key(especialidad, personal, ambulatorio),
+    foreign key(especialidad) references especialidad(nombre) on delete cascade on update cascade,
+    foreign key(ambulatorio, personal) references personalSanitario(ambulatorio, dni) on delete cascade on update cascade
+);
+
+create table consulta (
+    identificador int NOT NULL,
+    ambulatorio int NOT NULL DEFAULT currval('secAmbulatorios'),
+    especialidad varchar(20) NOT NULL,
+    primary key(identificador, ambulatorio),
+    foreign key(ambulatorio) references ambulatorio(codigoAmbulatorio) on delete restrict on update cascade,
+    foreign key(especialidad) references especialidad(nombre) on delete restrict on update cascade
+);
+
+create table pertenecer (
+    ambulatorioPersonal int NOT NULL DEFAULT currval('secAmbulatorios'),
+    personal char(9) NOT NULL,
+    ambulatorioConsulta int NOT NULL DEFAULT currval('secAmbulatorios'),
+    consulta int NOT NULL,
+    check (ambulatorioPersonal = ambulatorioConsulta),
+    primary key(ambulatorioPersonal, personal, ambulatorioConsulta, consulta),
+    foreign key(ambulatorioPersonal, personal) references personalSanitario(ambulatorio, dni) on delete cascade on update cascade,
+    foreign key(ambulatorioConsulta, consulta) references consulta(ambulatorio, identificador) on delete cascade on update cascade
+);
+
+create table enfermedad (
+    nombre varchar(30) NOT NULL,
+    descripcion varchar(200),
+    primary key(nombre)
+);
+
+create table paciente (
+    cip varchar(14) NOT NULL,
+    dni char(9) NOT NULL,
+    numSeguridadSocial int NOT NULL,
+    nombre varchar(60) NOT NULL,
+    fechaNacimiento date NOT NULL,
+    sexo varchar(12) NOT NULL,
+    grupoSanguineo varchar(3) NOT NULL,
+    nacionalidad VARCHAR(40) NOT NULL DEFAULT 'Española',
+    direccion varchar(50) NOT NULL,
+    telefono int NOT NULL,
+    primary key(cip)
+);
+
+create table tenerEnfermedad(
+    enfermedad varchar(30) NOT NULL,
+    paciente varchar(14) NOT NULL,
+    primary key(enfermedad, paciente),
+    foreign key(enfermedad) references enfermedad(nombre) on delete cascade on update cascade,
+    foreign key(paciente) references paciente(cip) on delete cascade on update cascade
+);
+
+create table tipoCita(
+    nombre varchar(30) NOT NULL,
+    especialidad varchar(20) NOT NULL,
+    descripcion varchar(200),
+    primary key(nombre, especialidad),
+    foreign key(especialidad) references especialidad(nombre) on delete restrict on update restrict
+);
+
+create table cita (
+    fechaHoraInicio timestamp NOT NULL,
+    fechaHoraFin timestamp CHECK (fechaHoraInicio < fechaHoraFin),
+    paciente varchar(14) NOT NULL,
+    consulta int NOT NULL,
+    ambulatorio int NOT NULL DEFAULT currval('secAmbulatorios'),
+    tipo varchar(30) NOT NULL,
+    especialidad varchar(20) NOT NULL,
+    primary key(fechaHoraInicio, paciente, consulta, ambulatorio),
+    foreign key (paciente) references paciente(cip) on delete restrict on update cascade,
+    foreign key (consulta, ambulatorio) references consulta(identificador, ambulatorio) on delete restrict on update cascade,
+    foreign key (tipo, especialidad) references tipoCita(nombre, especialidad) on delete restrict on update cascade
+);
+
+create table urgencia (
+    cita timestamp NOT NULL,
+    paciente varchar(14) NOT NULL,
+    consulta int NOT NULL,
+    ambulatorio int NOT NULL DEFAULT currval('secAmbulatorios'),
+    soborno decimal(6, 2) DEFAULT 0,
+    gravedad int NOT NULL DEFAULT 1 CHECK(gravedad BETWEEN 1 AND 10),
+    primary key(cita, paciente, consulta, ambulatorio),
+    foreign key (cita, paciente, consulta, ambulatorio) references cita(fechaHoraInicio, paciente, consulta, ambulatorio) on delete restrict on update cascade
+);
+
+create table medicamento(
+    nombre varchar(30) NOT NULL,
+    prospecto varchar(500) NOT NULL,
+    primary key(nombre)
+);
+
+create table receta (
+    cita timestamp NOT NULL,
+    paciente varchar(17) NOT NULL,
+    consulta int NOT NULL,
+    ambulatorio int NOT NULL DEFAULT currval('secAmbulatorios'),
+    codigoReceta int NOT NULL,
+    medicamento varchar(30) NOT NULL,
+    cantidad int NOT NULL DEFAULT 1,
+    descripcion varchar(200) NOT NULL,
+    fechaInicio date NOT NULL,
+    fechaFin date CHECK (fechaInicio < fechaFin),
+    primary key(codigoReceta, cita, paciente, ambulatorio),
+    foreign key (cita, paciente, consulta, ambulatorio) references cita(fechaHoraInicio, paciente, consulta, ambulatorio) on delete restrict on update cascade,
+    foreign key (medicamento) references medicamento(nombre) on delete restrict on update cascade
+);
+
+/* Función para asignar el codigo de la receta */
+create or replace function asignarCodigoReceta() returns trigger as $acr$
+begin
+	if exists (select r1.codigoReceta
+			   from receta r1
+			   where r1.cita = new.cita
+			   and r1.codigoReceta >= all (select codigoReceta
+										from receta r2
+										where r2.cita = new.cita))
+	then
+		begin
+			new.codigoReceta := (select r1.codigoReceta + 1
+								 from receta r1
+								 where r1.cita = new.cita
+								 and r1.codigoReceta >= all (select codigoReceta
+														  from receta r2
+														  where r2.cita = new.cita));
+		end;
+	else
+		begin
+			new.codigoReceta := 1;
+		end;
+	end if;
+	
+	return new;
+end;
+$acr$ language plpgsql;
+
+/* Trigger para insertar el número de receta */
+create trigger triggerCodigoReceta before insert on receta
+for each row execute procedure asignarCodigoReceta();
+
+create table derivarHospital (
+    cita timestamp NOT NULL,
+    paciente varchar(14) NOT NULL,
+    consulta int NOT NULL,
+    ambulatorio int NOT NULL DEFAULT currval('secAmbulatorios'),
+    hospital int NOT NULL DEFAULT currval('secHospitales'),
+    primary key(cita, paciente, consulta, ambulatorio, hospital),
+    foreign key (cita, paciente, consulta, ambulatorio) references cita(fechaHoraInicio, paciente, consulta, ambulatorio) on delete restrict on update cascade,
+    foreign key (hospital) references hospital(codigoHospital) on delete restrict on update cascade
+);
---
+++ Scripts/InsercionAmbulatorio.sql
@@ -0,0 +1,180 @@
+--ENFERMEDAD
+insert into enfermedad (nombre, descripcion)	
+	values 
+	('Mascaria', 'Malditos mosquitos.'),
+	('Gripe Clase A', 'Lo habitual.'),
+	('Minero', 'No hay una descripción disponible. Habrá que seguir investigando.'),
+	('CoD-02', 'Los videojuegos son el problema de la sociedad.');
+
+--PACIENTE
+insert into paciente (cip, dni, numSeguridadSocial, nombre, fechaNacimiento, sexo, grupoSanguineo, nacionalidad, direccion, telefono)
+	values 
+	('cip123', '56655665S', 5648, 'Martin Suarez', '2000-12-11', 'Masculino', '0+', 'Española', 'Pontevedra, Vigo', '655214698'),
+	('cip124', '56562262F', 5649, 'Ainhoa Vivel', '2000-03-07', 'Femenino', '0+', 'Española', 'Ciudad Falsa, Calle Falsa', '986252525'),
+	('cip125', '35383321F', 5650, 'Pablo Tarrio', '2000-12-27', 'Masculino', 'AB+', 'Española', 'Lugo, Lugo', '665541416');
+
+--TENER ENFERMEDAD
+insert into tenerEnfermedad (enfermedad, paciente)
+	values 
+	('Minero', 'cip124'),
+	('Gripe Clase A', 'cip123'),
+	('CoD-02', 'cip125');
+
+--ESPECIALIDAD
+insert into especialidad (nombre, descripcion) 
+	values
+	('General', 'Soy multivaluado!'),
+	('Cirujia', 'Dedicado a abrir gente.'), 
+	('Oftalmologia', 'Dedicado a la mirar los ojos.'),
+	('Odontologia', '¿Tienes caries?');
+
+--TIPO CITA
+insert into tipoCita (nombre, especialidad, descripcion)
+	values
+	('Urgencia', 'General', 'No te mueras aún!'),
+	('Consulta', 'Cirujia', 'Cita diaria.'),
+	('Consulta', 'Oftalmologia', 'No ve nada.');
+	
+--MEDICAMENTO
+insert into medicamento (nombre, prospecto)
+	values
+	('Acetaminofeno', 'Esta indicado para el alivio o tratamiento del dolor ocasional leve o moderado, como dolor de cabeza, dental, muscular (contracturas) o de espalda (lumbago) y en estados febriles.'),
+	('Antiagregante', 'Este medicamento esta indicado en el alivio sintomatico de los dolores ocasionales leves o moderados, como dolores de cabeza, dentales, menstruales, musculares (contracturas) o de espalda (lumbalgia) y en estados febriles.');
+
+--HOSPITALES
+insert into hospital (nombre, direccion, telefono, provincia)
+	values
+	('Hospital Universitario de Lucus Augusti', 'Rua Dr. Ulises Romero, 1, 27003 Lugo', '982296000', 'Lugo'),
+	('Polusa', 'Rua Iglesias Otero, s/n, 27004 Lugo', '982222854', 'Lugo'),
+	('Juan Carlos I', 'Valle de los Caidos', '193200000', 'Comunidad de Madrid');
+	
+--AMBULATORIO 1 e INSERCIONES ASOCIADAS
+insert into ambulatorio (nombre, direccionPostal, anoConstruccion, provincia)
+	values ('Fingoi', 'Rua Armorica, s/n, 27002 Lugo', '1950', 'Lugo');
+	
+insert into subvencion (fecha, cantidad, motivo)
+	values
+	('2019-12-23', 2000.05, 'Porque se lo merecen.'),
+	('2019-05-31', 5015.55, 'Porque no se lo merecen.');
+	
+insert into donativo (fecha, cantidad, donante)
+	values ('2020-02-03', 100000, 'Rafael Nadal');
+	
+insert into material (tipo, descripcion, coste, fechaCompra, medico)
+	values
+	('bisturi', 'Elemento quirurgico.', 12.05, '2020-03-12', TRUE),
+	('ordenador', 'Ordenador empleado por el usuario administrador.', 1299.99, '2012-09-25', FALSE);
+
+insert into asociado (hospital, distancia)
+	values
+	((select codigoHospital from hospital where nombre like 'Polusa' and provincia like 'Lugo'), 3.6),
+	((select codigoHospital from hospital where nombre like 'Hospital Universitario de Lucus Augusti' and provincia like 'Lugo'), 15.3); 
+
+insert into personalNoSanitario (dni, nombre, fechaIncorporacion, telefono, sueldo, tipo)
+	values ('52134865S', 'Clara Suarez', '2020-01-15', '687452133', 1674.23, 'secretario');
+insert into personalAdministrador (personal, contrasena)
+	values ('52134865S', 'sualci');
+	
+insert into personalNoSanitario (dni, nombre, fechaIncorporacion, telefono, sueldo, tipo)
+	values ('00000000A', 'a', '2000-01-01', '999999999', 0.01, 'dueño');
+insert into personalAdministrador (personal, contrasena)
+	values ('00000000A', 'a');
+
+insert into personalSanitario (dni, nombre, fechaIncorporacion, telefono, sueldo)
+	values ('84125961A', 'Adrian Vispalia', '2005-11-03', '981250503', 2000.01);
+insert into especializacionPersonal (especialidad, personal)
+	values
+	('Cirujia', '84125961A'),
+	('Oftalmologia', '84125961A');
+	
+insert into personalSanitario (dni, nombre, fechaIncorporacion, telefono, sueldo)
+	values ('52145961A', 'Miguel Torres', '2010-11-03', '985202020', 600.01);
+insert into especializacionPersonal (especialidad, personal)
+	values ('General', '52145961A');
+
+insert into consulta (identificador, especialidad)
+	values
+	(101, 'Cirujia'),
+	(203, 'Oftalmologia'),
+	(005, 'General');
+
+insert into pertenecer (personal, consulta) 
+	values
+	('84125961A', 101),
+	('84125961A', 203),
+	('52145961A', 005);
+	
+insert into cita (fechaHoraInicio, fechaHoraFin, paciente, consulta, tipo, especialidad)
+	values ('2019-10-12 12:22:23', '2019-10-12 13:05:00', 'cip124', 005, 'Urgencia', 'General');
+insert into urgencia (cita, paciente, consulta, soborno, gravedad)
+	values ('2019-10-12 12:22:23', 'cip124', 005, 0.01, 1);
+insert into receta (cita, paciente, consulta, medicamento, cantidad, descripcion, fechaInicio, fechaFin)
+	values ('2019-10-12 12:22:23', 'cip124', 005, 'Acetaminofeno', 3, 'Drogarse cada 6 horas.', '2003-06-24', '2009-01-02');
+insert into derivarHospital (cita, paciente, consulta)
+	values ('2019-10-12 12:22:23', 'cip124', 005);
+
+
+--AMBULATORIO 2 e INSERCIONES ASOCIADAS
+insert into ambulatorio (nombre, direccionPostal, anoConstruccion, provincia)
+	values('Galeras', 'Rua de Entrerrios, 3, 15705 Santiago de Compostela', '1920', 'A Coruña');
+
+insert into subvencion (fecha, cantidad, motivo)
+	values ('2010-06-28', 3000, 'Porque se lo merecen');
+	
+insert into donativo (fecha, cantidad, donante)
+	values
+	('2019-05-31', 100000, 'Amancio Ortega'),
+	(CURRENT_DATE, 100000, 'Pau Gasol');
+
+insert into material (tipo, descripcion, coste, fechaCompra, medico)
+	values
+	('bisturi', 'Elemento quirurgico.', 12.3, '2019-10-01', TRUE),
+	('ordenador', 'Ordenador empleado por el usuario administrador.',900.99 ,'2012-09-25', FALSE),
+	('camilla', 'Elemento de las consultas.', 400.0 ,'2018-03-09', TRUE);
+
+insert into asociado (hospital, distancia)
+	values ((select codigoHospital from hospital where nombre like 'Polusa' and provincia like 'Lugo'), 5.2);
+
+insert into personalNoSanitario (dni, nombre, fechaIncorporacion, telefono, sueldo, tipo)
+	values ('76589765T', 'Pablo Tarrio', '2019-05-30', '678543226', 1674.23, 'secretario');
+
+insert into personalAdministrador (personal,contrasena)
+	values ('76589765T','char');
+
+insert into personalNoSanitario (dni, nombre, fechaIncorporacion, telefono, sueldo, tipo)
+	values ('10000000A', 'p', '2000-01-01', '999999999', 0.01, 'dueño');
+insert into personalAdministrador (personal, contrasena)
+	values ('10000000A', 'p');
+
+insert into personalSanitario (dni, nombre, fechaIncorporacion, telefono, sueldo)
+	values ('83325741B', 'Marcos Henandez', '2001-12-23', '611280593', 20100.31);
+	
+insert into especializacionPersonal (especialidad, personal)
+	values
+	('Cirujia', '83325741B'),
+	('Oftalmologia', '83325741B');
+	
+insert into personalSanitario (dni, nombre, fechaIncorporacion, telefono, sueldo)
+	values ('83325331B', 'Humberto Gutierrez', '2012-10-12', '789649382', 430.00);
+insert into especializacionPersonal (especialidad, personal)
+	values ('General', '83325331B');
+
+insert into consulta (identificador, especialidad)
+	values
+	(203, 'Cirujia'),
+	(102, 'Oftalmologia'),
+	(305, 'General');
+
+insert into pertenecer (personal, consulta) 
+	values
+	('83325331B', 102),
+	('83325331B', 305);
+
+insert into cita (fechaHoraInicio, fechaHoraFin, paciente, consulta, tipo, especialidad)
+	values ('2019-10-12 12:21:23', '2019-10-12 13:05:00', 'cip123', 305, 'Urgencia', 'General');
+insert into urgencia (cita, paciente, consulta, soborno, gravedad)
+	values ('2019-10-12 12:21:23', 'cip123', 305, 0.01, 1);
+insert into receta (cita, paciente, consulta, medicamento, cantidad, descripcion, fechaInicio, fechaFin)
+	values ('2019-10-12 12:21:23', 'cip123', 305, 'Acetaminofeno', 3, 'Drogarse cada 6 horas.', '2003-06-24', '2009-01-02');
+insert into derivarHospital (cita, paciente, consulta)
+	values ('2019-10-12 12:21:23', 'cip123', 305);
