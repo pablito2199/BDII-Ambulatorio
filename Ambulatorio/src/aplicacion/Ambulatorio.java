f31354b3384ea62c16dc40293e24381a9c7629e9
TheMart_n <TheMartn@users.noreply.github.com>
2020-04-17 12:10:29 +0200

    Aplicacion
    Nuevas clases + implementacion de equals

---
+++ Ambulatorio/src/aplicacion/Ambulatorio.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion;
+
+import java.time.Year;
+
+/**
+ *
+ * @author Martín Suárez García
+ */
+public class Ambulatorio {
+
+    private Integer codigo;
+    private String nombre;
+    private String direccion;
+    private Year anoConstruccion;
+    private String provicia;
+    private String telefono;
+
+    //Constructor
+    public Ambulatorio(Integer codigo, String nombre, String direccion, Year anoConstruccion, String provicia, String telefono) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.anoConstruccion = anoConstruccion;
+        this.provicia = provicia;
+        this.telefono = telefono;
+    }
+
+    //Getters y Setters
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Year getAnoConstruccion() {
+        return anoConstruccion;
+    }
+
+    public void setAnoConstruccion(Year anoConstruccion) {
+        this.anoConstruccion = anoConstruccion;
+    }
+
+    public String getProvicia() {
+        return provicia;
+    }
+
+    public void setProvicia(String provicia) {
+        this.provicia = provicia;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Ambulatorio) {
+            return ((Ambulatorio) obj).getCodigo().equals(this.nombre);
+        }
+        return false;
+    }
+}
--- Ambulatorio/src/aplicacion/Paciente.java
+++ Ambulatorio/src/aplicacion/Paciente.java
@@ -7,13 +7,118 @@ import java.util.Date;
  * @author Martín Suárez García
  */
 public class Paciente {
-    
+
     private String cip;
     private String dni;
-    private int numSeguridadSocial;
+    private Integer numSeguridadSocial;
     private String nombre;
     private Date fechaNacimiento;
     private String sexo;
     private GrupoSangre grupo;
+    private String nacionalidad;
+    private String direccion;
+    private String telefono;
 
+    //Constructor
+    public Paciente(String cip, String dni, int numSeguridadSocial, String nombre, Date fechaNacimiento, String sexo, GrupoSangre grupo, String nacionalidad, String direccion, String telefono) {
+        this.cip = cip;
+        this.dni = dni;
+        this.numSeguridadSocial = numSeguridadSocial;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.sexo = sexo;
+        this.grupo = grupo;
+        this.nacionalidad = nacionalidad;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    //Getters y Setters
+    public String getCip() {
+        return cip;
+    }
+
+    public void setCip(String cip) {
+        this.cip = cip;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getNumSeguridadSocial() {
+        return numSeguridadSocial;
+    }
+
+    public void setNumSeguridadSocial(int numSeguridadSocial) {
+        this.numSeguridadSocial = numSeguridadSocial;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public GrupoSangre getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(GrupoSangre grupo) {
+        this.grupo = grupo;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Paciente) {
+            return ((Paciente) obj).getCip().equals(this.cip);
+        }
+        return false;
+    }
 }
