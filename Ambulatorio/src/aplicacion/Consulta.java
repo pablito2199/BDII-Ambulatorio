db08ad17a3e65cfb94965ba628cf2e22ef621cd7
TheMart_n <TheMartn@users.noreply.github.com>
2020-04-17 13:02:37 +0200

    Aplicacion
    Nuevas clases

---
+++ Ambulatorio/src/aplicacion/Consulta.java
@@ -0,0 +1,52 @@
+package aplicacion;
+
+/**
+ *
+ * @author Martín Suárez García
+ */
+public class Consulta {
+
+    private Integer identificador;
+    private Integer ambulatorio;
+    private String especialidad;
+
+    //Constructor
+    public Consulta(Integer identificador, Integer ambulatorio, String especialidad) {
+        this.identificador = identificador;
+        this.ambulatorio = ambulatorio;
+        this.especialidad = especialidad;
+    }
+
+    //Getters y Setters
+    public Integer getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(Integer identificador) {
+        this.identificador = identificador;
+    }
+
+    public Integer getAmbulatorio() {
+        return ambulatorio;
+    }
+
+    public void setAmbulatorio(Integer ambulatorio) {
+        this.ambulatorio = ambulatorio;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Consulta) {
+            return ((Consulta) obj).getIdentificador().equals(this.identificador);
+        }
+        return false;
+    }
+}
