51bd1611ffa4827cf1213e3643d4206111fd79a6
TheMart_n <TheMartn@users.noreply.github.com>
2020-05-01 11:10:29 +0200

    Update DAOCitas.java

--- Ambulatorio/src/baseDatos/DAOCitas.java
+++ Ambulatorio/src/baseDatos/DAOCitas.java
@@ -414,7 +414,12 @@ public class DAOCitas extends AbstractDAO {
                     + "and ci.fechaHoraInicio < ? "
                     + "and ci.fechaHoraFin is null"
                     + "and ci.consulta = ? "
-                    + "and co.ambulatorio = ? "
+                    + "and ci.ambulatorio = ? "
+                    + "and not exists (select u.cita "
+                    + "from urgencia as u "
+                    + "where u.cita = ci.fechaHoraInicio "
+                    + "and ci.consulta = u.consulta "
+                    + "and ci.ambulatorio = u.ambulatorio) "
                     + "order by fechaHoraInicio asc"
             );
 
