3b96ab497d8cf3f5f55b6c772e6d8e8259902df9
Noah <63094919+ainhoaVivel@users.noreply.github.com>
2020-05-04 21:53:12 +0200

    Cambios en VPacientes (otra vez)

--- Ambulatorio/src/gui/VGestionEnfermedades.java
+++ Ambulatorio/src/gui/VGestionEnfermedades.java
@@ -295,7 +295,6 @@ public class VGestionEnfermedades extends javax.swing.JDialog {
     //Botón Regresar, vuelve a la ventana anterior, cerrando la ventana actual
     private void btnRegresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegresarActionPerformed
         //Busca los pacientes de la ventana anterior
-        padre.buscarPacientes();
         //Cerramos la ventana actual
         this.dispose();
     }//GEN-LAST:event_btnRegresarActionPerformed
