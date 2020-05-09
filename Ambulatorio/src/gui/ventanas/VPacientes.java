2c0f1e19579523dcda1bcda5156d17a5a87fa575
Noah <63094919+ainhoaVivel@users.noreply.github.com>
2020-05-09 14:29:00 +0200

    VPacientes

--- Ambulatorio/src/gui/ventanas/VPacientes.java
+++ Ambulatorio/src/gui/ventanas/VPacientes.java
@@ -145,7 +145,6 @@ public class VPacientes extends javax.swing.JDialog {
 
         tablaPacientes.setModel(new ModeloTablaPacientes());
         tablaPacientes.setToolTipText("Tabla de pacientes");
-        tablaPacientes.setAutoscrolls(false);
         tablaPacientes.getTableHeader().setReorderingAllowed(false);
         tablaPacientes.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
